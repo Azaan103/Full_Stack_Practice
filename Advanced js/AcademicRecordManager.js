2e78e9cd8a71e23c6acf0672fc72b4bd9516fd5c
Azaan1003 <iamazaanrahim@gmail.com>
2026-07-14 15:23:45 +0500

    Academic record manager completed

--- Advanced js/AcademicRecordManager.js
+++ Advanced js/AcademicRecordManager.js
@@ -2,9 +2,11 @@ let stu_name_input = document.querySelector("#studentName")
 let stu_marks_input = document.querySelector("#studentMarks")
 let stu_table = document.querySelector("#studentTable")
 let addbtn = document.querySelector("#addBtn")
-ID= 0
-function idgenerator()
-{
+let fndbtn = document.querySelector("#findBtn")
+let passedBtn = document.querySelector("#passedBtn")
+ID = 241160
+
+function idgenerator() {
     return ID++
 }
 
@@ -12,13 +14,19 @@ const students =
     [
         { ID: idgenerator(), Name: "Azaan", Marks: 80, Grade: 'A-', Status: "Active" },
         { ID: idgenerator(), Name: "Aazmir", Marks: 60, Grade: 'C+', Status: "Active" },
+        { ID: idgenerator(), Name: "Hunaiba", Marks: 90, Grade: 'A+', Status: "Active" },
+        { ID: idgenerator(), Name: "Alice", Marks: 30, Grade: 'F', Status: "Active" },
         { ID: idgenerator(), Name: "Hani", Marks: 10, Grade: 'F', Status: "Active" }
     ]
 
 display();
 
+//                DISPLAY STUDENT TABLE 
 function display() {
+
+    stu_table.innerHTML = "";
     students.forEach(function (student) {
+
         stu_table.innerHTML += `
         <tr>
             <td>${student.ID}</td>
@@ -29,19 +37,62 @@ function display() {
         </tr>
     `;
     })
-
 }
 
-
+//                ADD STUDENT LOGIC
 addbtn.addEventListener('click', function () {
     let newstudentname = stu_name_input.value
     let newstudentmarks = stu_marks_input.value
 
-
-    let newdata = { ID: 4, Name: newstudentname, Marks: newstudentmarks, Grade: 'N/A', Status: "Not_Active" }
+    let newdata = { ID: idgenerator(), Name: newstudentname, Marks: newstudentmarks, Grade: 'N/A', Status: "Not_Active" }
     students.push(newdata)
 
     display();
 })
 
+fndbtn.addEventListener('click', function () {
 
+    let foundstu = students.find(function (student) {
+        let newstudentname = stu_name_input.value
+        return student.Name == newstudentname
+    })
+    if (foundstu) {
+
+        stu_table.innerHTML = "";
+
+        stu_table.innerHTML += `
+        <tr>
+            <td>${foundstu.ID}</td>
+            <td>${foundstu.Name}</td>
+            <td>${foundstu.Marks}</td>
+            <td>${foundstu.Grade}</td>
+            <td>${foundstu.Status}</td>
+        </tr>
+    `;
+    }
+    else alert("Student Not found")
+})
+
+passedBtn.addEventListener('click', function () {
+
+    let findpassstu = students.filter(function (student) {
+        return student.Marks > 50 
+    })
+
+        stu_table.innerHTML = "";
+    findpassstu.forEach(function(student){
+
+        
+        stu_table.innerHTML += `
+        <tr>
+             <td>${student.ID}</td>
+            <td>${student.Name}</td>
+            <td>${student.Marks}</td>
+            <td>${student.Grade}</td>
+            <td>${student.Status}</td>
+        </tr>
+    `;
+    })
+
+
+})
