ecb441ff69b9e6de1f9a1f2eec2408a5b9dae5e5
Azaan1003 <iamazaanrahim@gmail.com>
2026-07-24 00:47:10 +0500

    ToDoApp

--- Advanced js/Projects/ToDoApp.js
+++ Advanced js/Projects/ToDoApp.js
@@ -6,13 +6,27 @@ const taskInput = document.querySelector("#taskInput");
 const priorityInput = document.querySelector("#priority");
 const dateInput = document.querySelector("#date");
 const addBtn = document.querySelector("#addBtn");
-
-
+const active = document.querySelector('#activeFilter')
+const completed = document.querySelector('#completedFilter')
+const allTasks = document.querySelector('#allFilter')
 const taskContainer = document.querySelector("#taskContainer");
+const totalTasks = document.querySelector("#totalTasks");
+const completedTasks = document.querySelector("#completedTasks");
+const pendingTasks = document.querySelector("#pendingTasks");
 
+function updateStats(){
+
+    totalTasks.textContent = tasks.length;
+
+    completedTasks.textContent = tasks.filter(task => task.completed).length;
+
+    pendingTasks.textContent = tasks.filter(task => !task.completed).length;
+
+}
 // =========================
 // Data
 // =========================
+
 
 let tasks = [
     {
@@ -35,18 +49,19 @@ let tasks = [
 // Display Tasks
 // =========================
 
-function displayTasks() {
+function displayTasks(taskList = tasks) {
 
     taskContainer.innerHTML = "";
 
-    tasks.forEach((task) => {
+    taskList.forEach((task) => {
 
         const priorityClass = task.priority.toLowerCase();
 
         taskContainer.innerHTML += `
-        <div class="task">
+        
+      <div class="task ${task.completed ? "completed" : ""}">
 
-            <div class="left">
+        <div class="left">
 
         <input
         type="checkbox"
@@ -54,35 +69,37 @@ function displayTasks() {
         data-id="${task.id}"
         ${task.completed ? "checked" : ""}>
 
-                <div>
+        <div>
 
-                    <h3>${task.title}</h3>
+            <h3>${task.title}</h3>
 
-                    <div class="meta">
+            <div class="meta">
 
-                        <div class="tag ${priorityClass}">
-                            ${task.priority}
-                        </div>
+                <div class="tag ${priorityClass}">
+                    ${task.priority}
+                </div>
 
-                        <div class="tag">
-                            ${task.date}
-                        </div>
-
-                    </div>
-
+                <div class="tag">
+                    ${task.date}
                 </div>
 
             </div>
 
-            <div class="actions">
-
-                <button class="secondary">Edit</button>
-
-                <button class="delete" data-id="${task.id}"> Delete </button>
-
-            </div>
-
         </div>
+
+    </div>
+
+    <div class="actions">
+
+        <button class="secondary">Edit</button>
+
+        <button class="delete" data-id="${task.id}">
+            Delete
+        </button>
+
+    </div>
+
+</div>
         `;
 
     });
@@ -114,6 +131,7 @@ function addTask() {
     tasks.push(task);
 
     displayTasks();
+    updateStats()
 
     taskInput.value = "";
     priorityInput.value = "Medium";
@@ -132,23 +150,69 @@ taskContainer.addEventListener("click", (e) => {
 
     if (e.target.classList.contains("delete")) {
         const deletion_id = Number(e.target.dataset.id)
-       tasks =  tasks.filter((task) => {
+        tasks = tasks.filter((task) => {
             return task.id != deletion_id
         })
         displayTasks();
+        updateStats();
     }
 })
 
+// =========================
+// Checkbox tick and cross
+// =========================
 taskContainer.addEventListener("click", (e) => {
+
     if (e.target.classList.contains("checkbox")) {
 
+        const id = Number(e.target.dataset.id);
+
+        const task = tasks.find(task => task.id === id);
+
+        task.completed = !task.completed;
+        displayTasks();
+        updateStats();
+
+    }
+
+});
+
+// =========================
+// Completed Tasks
+// =========================
+
+active.addEventListener("click",()=>{
+    const activetasks = tasks.filter(task => task.completed != true)
+    
+    displayTasks(activetasks)
+    updateStats();
+})
+
+// =========================
+//  Completed Tasks
+// =========================
+
+completed.addEventListener("click",()=>{
+    const completedtasks = tasks.filter(task => task.completed == true)
+    
+    displayTasks(completedtasks)
+    updateStats();
+})
+
+// =========================
+//  All Tasks
+// =========================
+
+allTasks.addEventListener("click",()=>{
+        displayTasks()
+        updateStats();
+})
 
 // =========================
 // Initial Render
 // =========================
 
 displayTasks();
-
-
+updateStats();
 
 
