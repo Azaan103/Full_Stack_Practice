b8dad0a7d7a8cb20a04328b139a5479514feab42
Azaan1003 <iamazaanrahim@gmail.com>
2026-08-08 15:04:46 +0500

    Use effect use

--- React/React-Vite/src/App.jsx
+++ React/React-Vite/src/App.jsx
@@ -1,65 +1,81 @@
-import Header from "./components/Header"
-import AddGameForm from "./components/AddGameForm"
-import GameList from "./components/Gamelist"
-import { useState } from "react";
+import Header from "./components/Header";
+import AddGameForm from "./components/AddGameForm";
+import GameList from "./components/Gamelist";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [games,setGames] = useState ([
-  {
-    id:1,
-    title: "Cyberpunk 2075",
-    genre: "RPG",
-    status: "Playing"
-  },
-  {
-    id:2,
-    title: "GTA V",
-    genre: "Action",
-    status: "Completed"
-  },
-  {
-    id:3,
-    title: "Minecraft",
-    genre: "Sandbox",
-    status: "Wishlist"
-  }
-]
-);
-function completeGame(id)
-{
-  const completedGame = games.map((game) => {
-    if ( game.id == id ) {
-       return {
-       ...game,
-    status: "Completed"
-}
+  const [games, setGames] = useState([
+    {
+      id: 1,
+      title: "Cyberpunk 2075",
+      genre: "RPG",
+      status: "Playing"
+    },
+    {
+      id: 2,
+      title: "GTA V",
+      genre: "Action",
+      status: "Completed"
+    },
+    {
+      id: 3,
+      title: "Minecraft",
+      genre: "Sandbox",
+      status: "Wishlist"
     }
+  ]);
 
-    return game;
-})
-  setGames(completedGame)
-}
+  function completeGame(id) {
+    const completedGame = games.map((game) => {
+      if (game.id == id) {
+        return {
+          ...game,
+          status: "Completed"
+        };
+      }
 
-function handleDeleteGame(id) {
- const updatedgamelist = games.filter((game)=> game.id != id)
- setGames(updatedgamelist)
-}
+      return game;
+    });
+
+    setGames(completedGame);
+  }
+
+  function handleDeleteGame(id) {
+    const updatedGameList = games.filter((game) => game.id != id);
+    setGames(updatedGameList);
+  }
+
+  // Save games whenever games changes
+  useEffect(() => {
+    localStorage.setItem("games", JSON.stringify(games));
+  }, [games]);
+
+  // Load games when app starts
+  useEffect(() => {
+    const savedGames = localStorage.getItem("games");
+
+    if (savedGames) {
+      setGames(JSON.parse(savedGames));
+    }
+  }, []);
+
   return (
-     <>
-      <Header 
-      totalGames = {games.length} />
-      <AddGameForm 
-      games = {games} 
-      setGames = {setGames}
+    <>
+      <Header totalGames={games.length} />
+
+      <AddGameForm
+        games={games}
+        setGames={setGames}
       />
 
-      <GameList   
-      games = {games}
-      onDelete = {handleDeleteGame} 
-      onComplete = {completeGame}/>
+      <GameList
+        games={games}
+        onDelete={handleDeleteGame}
+        onComplete={completeGame}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
